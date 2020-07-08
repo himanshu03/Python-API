d0975eb71da7da36903955c66b04a3eaec8b7e20
Mamta Giri <I0307@INNOVACCER.IN>
2020-07-09 00:02:17 +0530

    st lukes updated

--- Care_Management/incare_care_management_test.py
+++ Care_Management/incare_care_management_test.py
@@ -498,3 +498,429 @@ def test_InCare_Care_Management_18(Authorization, Base_Url,set_cookie):
 		print(tc_desc + "Status:- " + tc_status)
 		conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
 
+
+def test_InCare_Care_Management_19(Authorization, Base_Url,set_cookie):
+	tc_desc = "Verify user is able to re-edit the completed task (completed after editing)"
+	tc_status = "FAIL"
+	tc_name = "Care_Management_TC019"
+	tc_priority = "Normal"
+	print(tc_desc + " is Executing")
+	try:
+		url = Base_Url + test_data['test_care'] + project_data[conftest.cmd_arg]["test_01"]
+		payload = {}
+		headers = {
+			'Authorization': Authorization,
+			'Content-Type': 'application/json',
+			'Cookie': set_cookie
+		}
+		response = requests.request("GET", url, headers=headers, data=payload)
+		assert response.status_code == 200
+		tc_status = "PASS"
+	except Exception as e:
+		tc_status = "FAIL"
+		print(e)
+		raise
+	finally:
+		print(tc_desc + "Status:- " + tc_status)
+		conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
+
+def test_InCare_Care_Management_20(Authorization, Base_Url,set_cookie):
+	tc_desc = "Verify edit button on closed care protocol"
+	tc_status = "FAIL"
+	tc_name = "Care_Management_TC02"
+	tc_priority = "Normal"
+	print(tc_desc + " is Executing")
+	try:
+		url = Base_Url + test_data['test_care'] + project_data[conftest.cmd_arg]["test_02"]
+		payload = {}
+		headers = {
+			'Authorization': Authorization,
+			'Content-Type': 'application/json',
+			'Cookie': set_cookie
+		}
+		response = requests.request("GET", url, headers=headers, data=payload)
+		assert response.status_code == 200
+		tc_status = "PASS"
+	except Exception as e:
+		tc_status = "FAIL"
+		print(e)
+		raise
+	finally:
+		print(tc_desc + "Status:- " + tc_status)
+		conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
+
+def test_InCare_Care_Management_21(Authorization, Base_Url,set_cookie):
+	tc_desc = "Verify the status of completed Engagement/Task should become editable, if the user re-open any Care protocol"
+	tc_status = "FAIL"
+	tc_name = "Care_Management_TC03"
+	tc_priority = "Normal"
+	print(tc_desc + " is Executing")
+	try:
+		url = Base_Url + test_data['test_care'] + project_data[conftest.cmd_arg]["test_03"]
+		payload = {}
+		headers = {
+			'Authorization': Authorization,
+			'Content-Type': 'application/json',
+			'Cookie': set_cookie
+		}
+		response = requests.request("GET", url, headers=headers, data=payload)
+		assert response.status_code == 200
+		tc_status = "PASS"
+	except Exception as e:
+		tc_status = "FAIL"
+		print(e)
+		raise
+	finally:
+		print(tc_desc + "Status:- " + tc_status)
+		conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
+
+def test_InCare_Care_Management_22(Authorization, Base_Url,set_cookie):
+	tc_desc = "To verify the available activities in care management_Adhoc_Tasks"
+	tc_status = "FAIL"
+	tc_name = "Care_Management_TC04"
+	tc_priority = "Normal"
+	print(tc_desc + " is Executing")
+	try:
+		url = Base_Url + test_data['test_care'] + project_data[conftest.cmd_arg]["test_04"]
+		payload = {}
+		headers = {
+			'Authorization': Authorization,
+			'Content-Type': 'application/json',
+			'Cookie': set_cookie
+		}
+		response = requests.request("GET", url, headers=headers, data=payload)
+		assert response.status_code == 200
+		tc_status = "PASS"
+	except Exception as e:
+		tc_status = "FAIL"
+		print(e)
+		raise
+	finally:
+		print(tc_desc + "Status:- " + tc_status)
+		conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
+
+def test_InCare_Care_Management_23(Authorization, Base_Url,set_cookie):
+	tc_desc = "Verify User is able to click on send button after typing the message"
+	tc_status = "FAIL"
+	tc_name = "Care_Management_TC05"
+	tc_priority = "Normal"
+	print(tc_desc + " is Executing")
+	try:
+		url = Base_Url + test_data['test_care'] + project_data[conftest.cmd_arg]["test_05"]
+		payload = {}
+		headers = {
+			'Authorization': Authorization,
+			'Content-Type': 'application/json',
+			'Cookie': set_cookie
+		}
+		response = requests.request("GET", url, headers=headers, data=payload)
+		assert response.status_code == 200
+		tc_status = "PASS"
+	except Exception as e:
+		tc_status = "FAIL"
+		print(e)
+		raise
+	finally:
+		print(tc_desc + "Status:- " + tc_status)
+		conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
+
+
+def test_InCare_Care_Management_24(Authorization, Base_Url,set_cookie):
+	tc_desc = "Verify the User is able to Add call Note"
+	tc_status = "FAIL"
+	tc_name = "Care_Management_TC06"
+	tc_priority = "Normal"
+	print(tc_desc + " is Executing")
+	try:
+		url = Base_Url + test_data['test_care'] + project_data[conftest.cmd_arg]["test_06"]
+		payload = "{\n    \"type\": \"CALL\",\n    \"text\": \"API Health check for call note\"\n}"
+		headers = {
+			'Authorization': Authorization,
+			'Content-Type': 'application/json',
+			'Cookie': set_cookie
+		}
+		response = requests.request("POST", url, headers=headers, data=payload)
+		assert response.status_code == 200 or 201
+		assert(response.json()['text']) == test_data['test_06']['text']
+		assert (response.json()['noteType']) == test_data['test_06']['noteType']
+		tc_status = "PASS"
+	except Exception as e:
+		tc_status = "FAIL"
+		print(e)
+		raise
+	finally:
+		print(tc_desc + "Status:- " + tc_status)
+		conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
+
+
+def test_InCare_Care_Management_25(Authorization, Base_Url,set_cookie):
+	tc_desc = "Verify the functionality of Export icon"
+	tc_status = "FAIL"
+	tc_name = "Care_Management_TC07"
+	tc_priority = "Normal"
+	print(tc_desc + " is Executing")
+	try:
+		url = Base_Url + test_data['test_care'] + project_data[conftest.cmd_arg]["test_06"]
+		payload = "{\n    \"type\": \"VISIT\",\n    \"text\": \"Visit Ad-hoc Task\",\n    \"visitType\": \"HRA (Health Risk Assessment)\"\n}"
+		headers = {
+			'Authorization': Authorization,
+			'Content-Type': 'application/json',
+			'Cookie': set_cookie
+		}
+		response = requests.request("POST", url, headers=headers, data=payload)
+		assert response.status_code == 200 or 201
+		assert(response.json()['text']) == test_data['test_07']['text']
+		assert (response.json()['metadata']['visitType'])== test_data['test_07']['visitType']
+		assert (response.json()['noteType']) == test_data['test_07']['noteType']
+		tc_status = "PASS"
+	except Exception as e:
+		tc_status = "FAIL"
+		print(e)
+		raise
+	finally:
+		print(tc_desc + "Status:- " + tc_status)
+		conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
+
+def test_InCare_Care_Management_26(Authorization, Base_Url,set_cookie):
+	tc_desc = "Verify the User is able to create Note Ad-hoc Task"
+	tc_status = "FAIL"
+	tc_name = "Care_Management_TC08"
+	tc_priority = "Normal"
+	print(tc_desc + " is Executing")
+	try:
+		url = Base_Url + test_data['test_care'] + project_data[conftest.cmd_arg]["test_06"]
+		payload = "{\n    \"type\": \"NOTE\",\n    \"text\": \"Ad-hoc Task \\\"API Health check for Note\\\"\"\n}"
+		headers = {
+			'Authorization': Authorization,
+			'Content-Type': 'application/json',
+			'Cookie': set_cookie
+		}
+		response = requests.request("POST", url, headers=headers, data=payload)
+		assert response.status_code == 200 or 201
+		assert(response.json()['text']) == test_data['test_08']['text']
+		assert (response.json()['noteType']) == test_data['test_08']['noteType']
+		tc_status = "PASS"
+	except Exception as e:
+		tc_status = "FAIL"
+		print(e)
+		raise
+	finally:
+		print(tc_desc + "Status:- " + tc_status)
+		conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
+
+
+def test_InCare_Care_Management_27(Authorization, Base_Url,set_cookie):
+	tc_desc = "Verify the User is able to create New Task"
+	tc_status = "FAIL"
+	tc_name = "Care_Management_TC09"
+	tc_priority = "Normal"
+	print(tc_desc + " is Executing")
+	try:
+		url = Base_Url + test_data['test_care'] + project_data[conftest.cmd_arg]["test_09"]["uri"]
+		payload = project_data[conftest.cmd_arg]["test_09"]["payload"]
+		headers = {
+			'Authorization': Authorization,
+			'Content-Type': 'application/json',
+			'Cookie': set_cookie
+		}
+		response = requests.request("POST", url, headers=headers, data=payload)
+		assert response.status_code == 200
+		assert(response.json()['status']) == test_data['test_09']['status']
+		assert(response.json()['data']['type']) == test_data['test_09']['type']
+		assert (response.json()['data']['note']) == test_data['test_09']['note']
+		assert (response.json()['data']['task']) == test_data['test_09']['task']
+		tc_status = "PASS"
+	except Exception as e:
+		tc_status = "FAIL"
+		print(e)
+		raise
+	finally:
+		print(tc_desc + "Status:- " + tc_status)
+		conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
+
+
+def test_InCare_Care_Management_48(Authorization, Base_Url,set_cookie):
+	tc_desc = "Select Care Protocol Unit in All Activity Filter"
+	tc_status = "FAIL"
+	tc_name = "Care_Management_TC12"
+	tc_priority = "Normal"
+	print(tc_desc + " is Executing")
+	try:
+		url = Base_Url + test_data['test_care'] + project_data[conftest.cmd_arg]["test_12"]
+		payload = {}
+		headers = {
+			'Authorization': Authorization,
+			'Content-Type': 'application/json',
+			'Cookie': set_cookie
+		}
+		response = requests.request("GET", url, headers=headers, data=payload)
+		assert response.status_code == 200
+		tc_status = "PASS"
+	except Exception as e:
+		tc_status = "FAIL"
+		print(e)
+		raise
+	finally:
+		print(tc_desc + "Status:- " + tc_status)
+		conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
+
+
+
+def test_InCare_Care_Management_49(Authorization, Base_Url,set_cookie):
+	tc_desc = "Select Task in All Activity Filter"
+	tc_status = "FAIL"
+	tc_name = "Care_Management_TC13"
+	tc_priority = "Normal"
+	print(tc_desc + " is Executing")
+	try:
+		url = Base_Url + test_data['test_care'] + project_data[conftest.cmd_arg]["test_13"]
+		payload = {}
+		headers = {
+			'Authorization': Authorization,
+			'Content-Type': 'application/json',
+			'Cookie': set_cookie
+		}
+		response = requests.request("GET", url, headers=headers, data=payload)
+		assert response.status_code == 200
+		tc_status = "PASS"
+	except Exception as e:
+		tc_status = "FAIL"
+		print(e)
+		raise
+	finally:
+		print(tc_desc + "Status:- " + tc_status)
+		conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
+
+
+
+def test_InCare_Care_Management_50(Authorization, Base_Url,set_cookie):
+	tc_desc = "Select Notes in All Activity Filter"
+	tc_status = "FAIL"
+	tc_name = "Care_Management_TC14"
+	tc_priority = "Normal"
+	print(tc_desc + " is Executing")
+	try:
+		url = Base_Url + test_data['test_care'] + project_data[conftest.cmd_arg]["test_14"]
+		payload = {}
+		headers = {
+			'Authorization': Authorization,
+			'Content-Type': 'application/json',
+			'Cookie': set_cookie
+		}
+		response = requests.request("GET", url, headers=headers, data=payload)
+		assert response.status_code == 200
+		tc_status = "PASS"
+	except Exception as e:
+		tc_status = "FAIL"
+		print(e)
+		raise
+	finally:
+		print(tc_desc + "Status:- " + tc_status)
+		conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
+
+
+
+def test_InCare_Care_Management_51(Authorization, Base_Url,set_cookie):
+	tc_desc = "Select call in All Activity Filter"
+	tc_status = "FAIL"
+	tc_name = "Care_Management_TC15"
+	tc_priority = "Normal"
+	print(tc_desc + " is Executing")
+	try:
+		url = Base_Url + test_data['test_care'] + project_data[conftest.cmd_arg]["test_15"]
+		payload = {}
+		headers = {
+			'Authorization': Authorization,
+			'Content-Type': 'application/json',
+			'Cookie': set_cookie
+		}
+		response = requests.request("GET", url, headers=headers, data=payload)
+		assert response.status_code == 200
+		tc_status = "PASS"
+	except Exception as e:
+		tc_status = "FAIL"
+		print(e)
+		raise
+	finally:
+		print(tc_desc + "Status:- " + tc_status)
+		conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
+
+
+
+def test_InCare_Care_Management_52(Authorization, Base_Url,set_cookie):
+	tc_desc = "Select Visit in All Activity Filter"
+	tc_status = "FAIL"
+	tc_name = "Care_Management_TC16"
+	tc_priority = "Normal"
+	print(tc_desc + " is Executing")
+	try:
+		url = Base_Url + test_data['test_care'] + project_data[conftest.cmd_arg]["test_16"]
+		payload = {}
+		headers = {
+			'Authorization': Authorization,
+			'Content-Type': 'application/json',
+			'Cookie': set_cookie
+		}
+		response = requests.request("GET", url, headers=headers, data=payload)
+		assert response.status_code == 200
+		tc_status = "PASS"
+	except Exception as e:
+		tc_status = "FAIL"
+		print(e)
+		raise
+	finally:
+		print(tc_desc + "Status:- " + tc_status)
+		conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
+
+
+
+def test_InCare_Care_Management_53(Authorization, Base_Url,set_cookie):
+	tc_desc = "Select Letters in All Activity Filter"
+	tc_status = "FAIL"
+	tc_name = "Care_Management_TC17"
+	tc_priority = "Normal"
+	print(tc_desc + " is Executing")
+	try:
+		url = Base_Url + test_data['test_care'] + project_data[conftest.cmd_arg]["test_17"]
+		payload = {}
+		headers = {
+			'Authorization': Authorization,
+			'Content-Type': 'application/json',
+			'Cookie': set_cookie
+		}
+		response = requests.request("GET", url, headers=headers, data=payload)
+		assert response.status_code == 200
+		tc_status = "PASS"
+	except Exception as e:
+		tc_status = "FAIL"
+		print(e)
+		raise
+	finally:
+		print(tc_desc + "Status:- " + tc_status)
+		conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
+
+
+def test_InCare_Care_Management_54(Authorization, Base_Url,set_cookie):
+	tc_desc = "Select Encounters in All Activity Filter"
+	tc_status = "FAIL"
+	tc_name = "Care_Management_TC18"
+	tc_priority = "Normal"
+	print(tc_desc + " is Executing")
+	try:
+		url = Base_Url + test_data['test_care'] + project_data[conftest.cmd_arg]["test_18"]
+		payload = {}
+		headers = {
+			'Authorization': Authorization,
+			'Content-Type': 'application/json',
+			'Cookie': set_cookie
+		}
+		response = requests.request("GET", url, headers=headers, data=payload)
+		assert response.status_code == 200
+		tc_status = "PASS"
+	except Exception as e:
+		tc_status = "FAIL"
+		print(e)
+		raise
+	finally:
+		print(tc_desc + "Status:- " + tc_status)
+		conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
