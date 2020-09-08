4a269299ad4255ed7dd0a03fb06962578f2e2f00
Himanshu Verma <verma.himanshu.27@gmail.com>
2020-09-08 13:48:28 +0530

    Add sanity annotation

--- Tests/worklist_test.py
+++ Tests/worklist_test.py
@@ -7,101 +7,13 @@ from numpy.random.mtrand import randint
 import conftest
 
 with open("Ymls/worklist_common.yml", "r") as common:
-	test_data = yaml.load(common)
+    test_data = yaml.load(common)
 
 with open("Ymls/worklist_project.yml", "r") as project:
-	project_data = yaml.load(project)
+    project_data = yaml.load(project)
 
-
-
-
-def test_Worklist_Todo(Authorization, Base_Url,set_cookie):
-	tc_desc = "To verify that the User can see his daily progress on User's Workqueue under Todos"
-	tc_status = "FAIL"
-	tc_name = "Worklist_TC01"
-	tc_priority = "Medium"
-
-	try:
-		url = Base_Url + test_data['test_care'] + project_data[conftest.cmd_arg]["test_01"]["uri"]
-		payload = project_data[conftest.cmd_arg]["test_01"]["payload"]
-		headers = {
-			'Authorization': Authorization,
-			'Content-Type': 'application/json',
-			'Cookie': set_cookie
-		}
-		response = requests.request("POST", url, headers=headers, data=payload)
-		assert response.status_code == 200
-		assert(response.json()['data']['allTodosCount'])>=0
-		assert (response.json()['data']['pendingTodosCount'])>=0
-		assert (response.json()['data']['completedTodosCount'])>=0
-
-		tc_status = "PASS"
-	except Exception as e:
-		tc_status = "FAIL"
-		print(test_data['test_01']['message'])
-		raise
-	finally:
-		conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
 
 @pytest.mark.sanity
-def test_Worklist_Scheduled_Task(Authorization, Base_Url,set_cookie):
-	tc_desc = "To verify the available scheduled task for current date"
-	tc_status = "FAIL"
-	tc_name = "Worklist_TC02"
-	tc_priority = "Medium"
-
-	try:
-		url = Base_Url + test_data['test_care'] + project_data[conftest.cmd_arg]["test_02"]["uri"]
-		payload = project_data[conftest.cmd_arg]["test_02"]["payload"]
-		headers = {
-			'Authorization': Authorization,
-			'Content-Type': 'application/json',
-			'Cookie': set_cookie
-		}
-		response = requests.request("POST", url, headers=headers, data=payload)
-		assert response.status_code == 200
-		#print(response.json()['data']['empi'])
-		# assert (response.json()['data']['pendingTodosCount'])>=0
-		# assert (response.json()['data']['completedTodosCount'])>=0
-
-		tc_status = "PASS"
-	except Exception as e:
-		tc_status = "FAIL"
-		print(test_data['test_02']['message'])
-		raise
-	finally:
-		conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
-
-
-
-def test_Worklist_03(Authorization, Base_Url,set_cookie):
-	tc_desc = "Submit assessment without any response"
-	tc_status = "FAIL"
-	tc_name = "Worklist_TC03"
-	tc_priority = "High"
-	print(tc_desc + " is Executing")
-	try:
-		url = Base_Url + test_data['test_care'] + project_data[conftest.cmd_arg]["test_03"]["uri"]
-		payload = project_data[conftest.cmd_arg]["test_03"]["payload"]
-		headers = {
-			'Authorization': Authorization,
-			'Content-Type': 'application/json',
-			'Cookie': set_cookie
-		}
-		response = requests.request("POST", url, headers=headers, data=payload)
-		assert response.status_code == 200
-		#assert(response.json()['status']) == test_data['test_09']['status']
-
-		tc_status = "PASS"
-	except Exception as e:
-		tc_status = "FAIL"
-		print(e)
-		raise
-	finally:
-		print(tc_desc + "Status:- " + tc_status)
-		conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
-
-
 def test_Worklist_TC01(Authorization, Base_Url, set_cookie):
     tc_desc = "To verify that the User can see his daily progress on User's Workqueue under Todos"
     tc_status = "FAIL"
@@ -118,9 +30,9 @@ def test_Worklist_TC01(Authorization, Base_Url, set_cookie):
         }
         response = requests.request("POST", url, headers=headers, data=payload)
         assert response.status_code == 200
-        assert(response.json()['data']['allTodosCount'])>=0
-        assert (response.json()['data']['pendingTodosCount'])>=0
-        assert (response.json()['data']['completedTodosCount'])>=0
+        assert (response.json()['data']['allTodosCount']) >= 0
+        assert (response.json()['data']['pendingTodosCount']) >= 0
+        assert (response.json()['data']['completedTodosCount']) >= 0
 
         tc_status = "PASS"
     except Exception as e:
@@ -130,6 +42,7 @@ def test_Worklist_TC01(Authorization, Base_Url, set_cookie):
     finally:
         conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
 
+@pytest.mark.sanity
 def test_Worklist_02(Authorization, Base_Url, set_cookie):
     tc_desc = "To verify the available scheduled task for current date"
     tc_status = "FAIL"
@@ -162,8 +75,8 @@ def test_Worklist_02(Authorization, Base_Url, set_cookie):
     finally:
         conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
 
-
-def test_Worklist_05(Authorization, Base_Url,set_cookie):
+@pytest.mark.sanity
+def test_Worklist_05(Authorization, Base_Url, set_cookie):
     tc_desc = "To verify the Click functionality of Scheduled Patient"
     tc_status = "FAIL"
     tc_name = "Worklist_TC05"
@@ -190,7 +103,8 @@ def test_Worklist_05(Authorization, Base_Url,set_cookie):
     finally:
         conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
 
-def test_Worklist_06(Authorization, Base_Url,set_cookie):
+@pytest.mark.sanity
+def test_Worklist_06(Authorization, Base_Url, set_cookie):
     tc_desc = "To Verify the Reassign functionality for Selected Task"
     tc_status = "FAIL"
     tc_name = "Worklist_TC06"
@@ -219,6 +133,7 @@ def test_Worklist_06(Authorization, Base_Url,set_cookie):
     finally:
         conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
 
+@pytest.mark.sanity
 def test_Worklist_07(Authorization, Base_Url, set_cookie):
     tc_desc = "To verify the pending task on Pending section under activity received"
     tc_status = "FAIL"
@@ -245,7 +160,8 @@ def test_Worklist_07(Authorization, Base_Url, set_cookie):
     finally:
         conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
 
-def test_Worklist_08(Authorization, Base_Url,set_cookie):
+@pytest.mark.sanity
+def test_Worklist_08(Authorization, Base_Url, set_cookie):
     tc_desc = "To verify all the Task on ALL section under activity received"
     tc_status = "FAIL"
     tc_name = "Worklist_TC08"
@@ -270,6 +186,7 @@ def test_Worklist_08(Authorization, Base_Url,set_cookie):
     finally:
         conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
 
+@pytest.mark.sanity
 def test_Worklist_09(Authorization, Base_Url, set_cookie):
     tc_desc = "To verify the pending task on Pending section under activity sent"
     tc_status = "FAIL"
@@ -297,7 +214,8 @@ def test_Worklist_09(Authorization, Base_Url, set_cookie):
     finally:
         conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
 
-def test_Worklist_10(Authorization, Base_Url,set_cookie):
+@pytest.mark.sanity
+def test_Worklist_10(Authorization, Base_Url, set_cookie):
     tc_desc = "To Verify the sort by drop down availability"
     tc_status = "FAIL"
     tc_name = "Worklist_TC10"
@@ -323,6 +241,7 @@ def test_Worklist_10(Authorization, Base_Url,set_cookie):
     finally:
         conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
 
+@pytest.mark.sanity
 def test_Worklist_11(Authorization, Base_Url, set_cookie):
     tc_desc = "Verify the CDPS risk sorting"
     tc_status = "FAIL"
@@ -361,8 +280,8 @@ def test_Worklist_11(Authorization, Base_Url, set_cookie):
     finally:
         conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
 
-
-def test_Worklist_12(Authorization, Base_Url,set_cookie):
+@pytest.mark.sanity
+def test_Worklist_12(Authorization, Base_Url, set_cookie):
     tc_desc = "Verify the search should not be reset after changing the date on worklist"
     tc_status = "FAIL"
     tc_name = "Worklist_TC12"
@@ -388,6 +307,7 @@ def test_Worklist_12(Authorization, Base_Url,set_cookie):
     finally:
         conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
 
+@pytest.mark.sanity
 def test_Worklist_13(Authorization, Base_Url, set_cookie):
     tc_desc = "Verify user is able to edit the message of sent activities from other user Activity sent section"
     tc_status = "FAIL"
@@ -411,7 +331,8 @@ def test_Worklist_13(Authorization, Base_Url, set_cookie):
             ids.append(output)
         print(ids)
 
-        url_1 = Base_Url + test_data['test_care'] + project_data[conftest.cmd_arg]["test_13"]["uri1"]+ids[0]+"/message"
+        url_1 = Base_Url + test_data['test_care'] + project_data[conftest.cmd_arg]["test_13"]["uri1"] + ids[
+            0] + "/message"
         print(url_1)
         payload_1 = project_data[conftest.cmd_arg]["test_13"]["payload1"]
         headers = {
@@ -430,7 +351,7 @@ def test_Worklist_13(Authorization, Base_Url, set_cookie):
     finally:
         conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
 
-
+@pytest.mark.sanity
 def test_Worklist_14(Authorization, Base_Url, set_cookie):
     tc_desc = "Verify user is able to reassign the activity to other user from activities sent of other User"
     tc_status = "FAIL"
@@ -448,11 +369,11 @@ def test_Worklist_14(Authorization, Base_Url, set_cookie):
         }
         response = requests.request("POST", url, headers=headers, data=payload)
         print(url)
-        #print(response.text)
+        # print(response.text)
         id = response.json()['data'][0]['id']
         print(id)
 
-        url_1 = Base_Url + test_data['test_care'] + project_data[conftest.cmd_arg]["test_14"]['uri']+id+"/_reassign"
+        url_1 = Base_Url + test_data['test_care'] + project_data[conftest.cmd_arg]["test_14"]['uri'] + id + "/_reassign"
         payload_1 = project_data[conftest.cmd_arg]["test_14"]["payload"]
         headers = {
             'Authorization': Authorization,
@@ -472,6 +393,7 @@ def test_Worklist_14(Authorization, Base_Url, set_cookie):
     finally:
         conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
 
+@pytest.mark.sanity
 def test_Worklist_15(Authorization, Base_Url, set_cookie):
     tc_desc = "Verify User is able to accept the received referral of other User"
     tc_status = "FAIL"
@@ -489,7 +411,7 @@ def test_Worklist_15(Authorization, Base_Url, set_cookie):
         }
         response = requests.request("POST", url, headers=headers, data=payload)
         print(url)
-        #print(response.text)
+        # print(response.text)
         # id = response.json()['data'][0]['id']
         # print(id)
         json_data = response.json()
@@ -499,7 +421,8 @@ def test_Worklist_15(Authorization, Base_Url, set_cookie):
             ids.append(output)
         print(ids)
 
-        url_1 = Base_Url + test_data['test_care'] + project_data[conftest.cmd_arg]["test_15"]['uri1']+ids[0]+"/_accept"
+        url_1 = Base_Url + test_data['test_care'] + project_data[conftest.cmd_arg]["test_15"]['uri1'] + ids[
+            0] + "/_accept"
         print(url_1)
         payload_1 = project_data[conftest.cmd_arg]["test_15"]["payload1"]
         print(payload_1)
@@ -521,6 +444,7 @@ def test_Worklist_15(Authorization, Base_Url, set_cookie):
     finally:
         conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
 
+@pytest.mark.sanity
 def test_Worklist_16(Authorization, Base_Url, set_cookie):
     tc_desc = "Verify User is able to Decline the received referral of other User"
     tc_status = "FAIL"
@@ -545,7 +469,8 @@ def test_Worklist_16(Authorization, Base_Url, set_cookie):
             ids.append(output)
         print(ids)
 
-        url_1 = Base_Url + test_data['test_care'] + project_data[conftest.cmd_arg]["test_16"]["uri1"]+ids[0]+"/_decline"
+        url_1 = Base_Url + test_data['test_care'] + project_data[conftest.cmd_arg]["test_16"]["uri1"] + ids[
+            0] + "/_decline"
         print(url_1)
         payload_1 = project_data[conftest.cmd_arg]["test_16"]["payload1"]
         print(payload_1)
@@ -556,7 +481,7 @@ def test_Worklist_16(Authorization, Base_Url, set_cookie):
         }
 
         response_1 = requests.request("PATCH", url_1, headers=headers, data=payload_1)
-        #print(response_1.json())
+        # print(response_1.json())
         assert response.status_code == 200
         assert response_1.status_code == 200
         tc_status = "PASS"
@@ -567,7 +492,7 @@ def test_Worklist_16(Authorization, Base_Url, set_cookie):
     finally:
         conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
 
-
+@pytest.mark.sanity
 def test_Worklist_17(Authorization, Base_Url, set_cookie):
     tc_desc = "To verify that the completed Task should be visible under Completed section"
     tc_status = "FAIL"
@@ -594,6 +519,7 @@ def test_Worklist_17(Authorization, Base_Url, set_cookie):
     finally:
         conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
 
+@pytest.mark.sanity
 def test_Worklist_18(Authorization, Base_Url, set_cookie):
     tc_desc = "Verify the Add Note funtionality for scheduled task"
     tc_status = "FAIL"
@@ -610,7 +536,7 @@ def test_Worklist_18(Authorization, Base_Url, set_cookie):
         }
         response = requests.request("POST", url, headers=headers, data=payload)
         print(url)
-        #print(response.text.encode('utf8'))
+        # print(response.text.encode('utf8'))
         assert response.status_code == 200
 
         tc_status = "PASS"
@@ -621,7 +547,7 @@ def test_Worklist_18(Authorization, Base_Url, set_cookie):
     finally:
         conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
 
-
+@pytest.mark.sanity
 def test_Worklist_19(Authorization, Base_Url, set_cookie):
     tc_desc = "Verify the patient is assigned as high priority on Single click"
     tc_status = "FAIL"
@@ -646,6 +572,7 @@ def test_Worklist_19(Authorization, Base_Url, set_cookie):
     finally:
         conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
 
+@pytest.mark.sanity
 def test_Worklist_20(Authorization, Base_Url, set_cookie):
     tc_desc = "Verify the patient is assigned as medium priority on Double click"
     tc_status = "FAIL"
@@ -670,6 +597,7 @@ def test_Worklist_20(Authorization, Base_Url, set_cookie):
     finally:
         conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
 
+@pytest.mark.sanity
 def test_Worklist_21(Authorization, Base_Url, set_cookie):
     tc_desc = "Verify User is able to remove priority on triple click"
     tc_status = "FAIL"
@@ -694,7 +622,8 @@ def test_Worklist_21(Authorization, Base_Url, set_cookie):
     finally:
         conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
 
-def test_Worklist_22(Authorization, Base_Url,set_cookie):
+@pytest.mark.sanity
+def test_Worklist_22(Authorization, Base_Url, set_cookie):
     tc_desc = "Verify User is able to Assign priority of multiple patient in One time"
     tc_status = "FAIL"
     tc_name = "Worklist_TC22"
@@ -718,6 +647,7 @@ def test_Worklist_22(Authorization, Base_Url,set_cookie):
     finally:
         conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
 
+@pytest.mark.sanity
 def test_Worklist_23(Authorization, Base_Url, set_cookie):
     tc_desc = "Verify User is able to Remove priority of multiple patient in One time"
     tc_status = "FAIL"
@@ -742,7 +672,8 @@ def test_Worklist_23(Authorization, Base_Url, set_cookie):
     finally:
         conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
 
-def test_Worklist_24(Authorization, Base_Url,set_cookie):
+@pytest.mark.sanity
+def test_Worklist_24(Authorization, Base_Url, set_cookie):
     tc_desc = "To verify the Care protocol filter"
     tc_status = "FAIL"
     tc_name = "Worklist_TC24"
@@ -768,6 +699,7 @@ def test_Worklist_24(Authorization, Base_Url,set_cookie):
     finally:
         conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
 
+@pytest.mark.sanity
 def test_Worklist_25(Authorization, Base_Url, set_cookie):
     tc_desc = "Verify the CDPS risk sorting"
     tc_status = "FAIL"
@@ -806,7 +738,7 @@ def test_Worklist_25(Authorization, Base_Url, set_cookie):
     finally:
         conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
 
-
+@pytest.mark.sanity
 def test_Worklist_26(Authorization, Base_Url, set_cookie):
     tc_desc = "Verify the HHSHCC risk sorting"
     tc_status = "FAIL"
@@ -845,6 +777,7 @@ def test_Worklist_26(Authorization, Base_Url, set_cookie):
     finally:
         conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
 
+@pytest.mark.sanity
 def test_Worklist_27(Authorization, Base_Url, set_cookie):
     tc_desc = "Verify the CDPS risk sorting"
     tc_status = "FAIL"
@@ -883,6 +816,7 @@ def test_Worklist_27(Authorization, Base_Url, set_cookie):
     finally:
         conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
 
+@pytest.mark.sanity
 def test_Worklist_28(Authorization, Base_Url, set_cookie):
     tc_desc = "Verify the CDPS risk sorting"
     tc_status = "FAIL"
@@ -921,28 +855,29 @@ def test_Worklist_28(Authorization, Base_Url, set_cookie):
     finally:
         conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
 
-def test_Worklist_29(Authorization, Base_Url,set_cookie):
-	tc_desc = "Verify the SVI risk sorting"
-	tc_status = "FAIL"
-	tc_name = "Worklist_TC29"
-	tc_priority = "High"
+@pytest.mark.sanity
+def test_Worklist_29(Authorization, Base_Url, set_cookie):
+    tc_desc = "Verify the SVI risk sorting"
+    tc_status = "FAIL"
+    tc_name = "Worklist_TC29"
+    tc_priority = "High"
 
-	try:
-		url = Base_Url + test_data['test_care'] + project_data[conftest.cmd_arg]["test_29"]["uri"]
-		print(url)
-		payload = project_data[conftest.cmd_arg]["test_29"]["payload"]
-		headers = {
-			'Authorization': Authorization,
-			'Content-Type': 'application/json',
-			'Cookie': set_cookie
-		}
-		response = requests.request("POST", url, headers=headers, data=payload)
-		assert response.status_code == 200
-		assert response.json()['metadata']['count'] >= 0
-		tc_status = "PASS"
-	except Exception as e:
-		tc_status = "FAIL"
-		print(test_data['test_29']['message'])
-		raise
-	finally:
-		conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
+    try:
+        url = Base_Url + test_data['test_care'] + project_data[conftest.cmd_arg]["test_29"]["uri"]
+        print(url)
+        payload = project_data[conftest.cmd_arg]["test_29"]["payload"]
+        headers = {
+            'Authorization': Authorization,
+            'Content-Type': 'application/json',
+            'Cookie': set_cookie
+        }
+        response = requests.request("POST", url, headers=headers, data=payload)
+        assert response.status_code == 200
+        assert response.json()['metadata']['count'] >= 0
+        tc_status = "PASS"
+    except Exception as e:
+        tc_status = "FAIL"
+        print(test_data['test_29']['message'])
+        raise
+    finally:
+        conftest.updatedb(tc_name, tc_desc, tc_status, tc_priority)
